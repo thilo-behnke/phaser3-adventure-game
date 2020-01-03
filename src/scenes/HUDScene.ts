a4391e81a732cff33467f981955bb447c6b76898
Thilo Behnke <jan-thilo.behnke@gmx.de>
2020-01-03 13:49:08 +0100

    cleanup

--- src/scenes/HUDScene.ts
+++ src/scenes/HUDScene.ts
@@ -28,39 +28,7 @@ export class HUDScene extends Phaser.Scene {
         this.playerPosObserver = new PositionObserver(
             gameObjectRegistry.getPlayer().sprite
         ).onReceive(this.updatePlayerPos);
-        /*        // Show player pos.
-        const player = gameObjectRegistry.getPlayer();
-        const playerPosSub = interval(100)
-            .pipe(
-                map(() => player.sprite.getCenter()),
-                tap((pos: Vector2) => {
-                    if (this.playerPosText) {
-                        this.playerPosText.destroy();
-                    }
-                    this.playerPosText = this.add.text(
-                        5,
-                        5,
-                        `Player - x: ${pos.x.toFixed(2)}, y: ${pos.y.toFixed(2)}`
-                    );
-                })
-            )
-            .subscribe();*/
 
-        // Show events.
-        /*        const eventSub = eventRegistry
-            .get()
-            .pipe(
-                map(events => takeRight(events, 5)),
-                tap(events => {
-                    if (this.eventText) {
-                        this.eventText.destroy();
-                    }
-                    this.eventText = this.add.text(10, SCREEN_HEIGHT - 100, events.join('\n'));
-                })
-            )
-            .subscribe();*/
-        /*        this.subscriptions = [playerPosSub];*/
-        // Grid.
         const [mapX, mapY] = sceneProvider.getMapDimensions();
         const gridLen = TILE_SIZE,
             gridColor = Color.GREY,
